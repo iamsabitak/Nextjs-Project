f7ce6f76dec7aae86b96fc566ef517092c21b631
sabita <sabitakhadka091@gmail.com>
2024-04-18 11:49:38 +0545

    learned about data fetching on client side

--- app/users/newusers/page.tsx
+++ app/users/newusers/page.tsx
@@ -1,7 +1,30 @@
 import React from "react";
+interface Users {
+  id: number;
+  name: string;
+  email: string;
+  address: {
+    city: string;
+    street: string;
+  };
+}
 
-const NewUserPage = () => {
-  return <div>NewUserPage</div>;
+const NewUserPage = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users: Users[] = await response.json();
+  return (
+    <div>
+      <h1>NewUserPage</h1>
+      <ul>
+        {users.map((user) => (
+          <div key={user.id}>
+            <li>{user.name}</li>
+            {/* <li>{user.address.city}</li> */}
+          </div>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default NewUserPage;
