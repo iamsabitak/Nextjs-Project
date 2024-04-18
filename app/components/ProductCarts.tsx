944883e680bebc29bc2baa610ee06a92a63016ef
sabita <sabitakhadka091@gmail.com>
2024-04-18 11:31:37 +0545

    learned about client and server side rendering

---
+++ app/components/ProductCarts.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import AddToCart from "./AddToCart";
+
+const ProductCarts = () => {
+  return (
+    <div>
+      <AddToCart />
+    </div>
+  );
+};
+export default ProductCarts;
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import Image from "next/image";
 import Link from "next/link";
+import ProductCarts from "./components/ProductCarts";
 
 export default function Home() {
   return (
@@ -7,6 +7,7 @@ export default function Home() {
       <h1>HELLO WORLD</h1>
       <Link href="/users">User</Link>
       <Link href="/users/newusers">NewUser</Link>
+      <ProductCarts />
     </main>
   );
 }
