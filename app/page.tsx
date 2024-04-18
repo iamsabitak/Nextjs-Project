748ec906d6abb3956b4b3a0ed98d00814ae79139
sabita <sabitakhadka091@gmail.com>
2024-04-18 11:18:53 +0545

    learned about new app routing

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
-import Image from 'next/image'
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>HELLO NEXTJS</h1>
+      <h1>HELLO WORLD</h1>
+      <Link href="/users">User</Link>
+      <Link href="/users/newusers">NewUser</Link>
     </main>
-  )
+  );
 }
