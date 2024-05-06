a4669da8ab8bef220806e3dc1256965cff8109ca
sabita <sabitakhadka091@gmail.com>
2024-05-06 17:02:14 +0545

    pushing the updated changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Link from "next/link";
 import ProductCarts from "./components/ProductCarts";
 import Navbar from "./components/Navbar";
+import HeroSection from "./components/hero/HeroSection";
 
 export default function Home() {
   return (
     <main>
       {/* <h1>HELLO WORLD</h1> */}
       <Navbar />
+      <HeroSection />
       {/* <Link href="/users">User</Link>
       <Link href="/users/newusers">NewUser</Link> */}
       {/* <ProductCarts /> */}
